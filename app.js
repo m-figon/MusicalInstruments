fd922ad5a4057fbc65683af123866ba0bee53bc9
mateusz <dalvan234@gmail.com>
2020-08-08 20:10:34 +0200

    tried to init angularjs bootstrap carousel

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let app = angular.module('app', ['ngRoute']);
+let app = angular.module('app', ['ngRoute','ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
 app.config(['$routeProvider', ($routeProvider) => {
     $routeProvider
@@ -19,6 +19,65 @@ app.config(['$routeProvider', ($routeProvider) => {
 app.controller('app-controller', ['$scope', '$http', ($scope, $http) => {
     $scope.message = "hello";
 }])
+
+app.controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  var slides = $scope.slides = [];
+  var currIndex = 0;
+
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: '//unsplash.it/' + newWidth + '/300',
+      text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+      id: currIndex++
+    });
+  };
+
+  $scope.randomize = function() {
+    var indexes = generateIndexesArray();
+    assignNewIndexesToSlides(indexes);
+  };
+
+  for (var i = 0; i < 4; i++) {
+    $scope.addSlide();
+  }
+
+  // Randomize logic below
+
+  function assignNewIndexesToSlides(indexes) {
+    for (var i = 0, l = slides.length; i < l; i++) {
+      slides[i].id = indexes.pop();
+    }
+  }
+
+  function generateIndexesArray() {
+    var indexes = [];
+    for (var i = 0; i < currIndex; ++i) {
+      indexes[i] = i;
+    }
+    return shuffle(indexes);
+  }
+
+  // http://stackoverflow.com/questions/962802#962890
+  function shuffle(array) {
+    var tmp, current, top = array.length;
+
+    if (top) {
+      while (--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+      }
+    }
+
+    return array;
+  }
+});
+
 
 app.controller('instruments-controller', ['$scope', '$routeParams', '$http', ($scope, $routeParams, $http) => {
     $scope.instrumentType = $routeParams.type;
