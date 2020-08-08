3cb22a0abad7e6327dd01de1d8089bf7414160d9
mateusz <dalvan234@gmail.com>
2020-08-08 16:16:50 +0200

    details controller and  display added, nav-bar appearance changed

--- app.js
+++ app.js
@@ -7,22 +7,41 @@ app.config(['$routeProvider', ($routeProvider) => {
         })
         .when('/instruments/:type', {
             templateUrl: 'views/instruments.html'
-            })
+        })
+        .when('/instruments/:type/:id', {
+            templateUrl: 'views/details.html'
+        })
         .otherwise({
-          redirectTo: '/home'
+            redirectTo: '/home'
         })
 }])
 
-app.controller('app-controller', ['$scope','$http',($scope,$http) => {
+app.controller('app-controller', ['$scope', '$http', ($scope, $http) => {
     $scope.message = "hello";
 }])
 
-app.controller('instrument-controller', ['$scope','$routeParams','$http',($scope,$routeParams,$http) => {
+app.controller('instruments-controller', ['$scope', '$routeParams', '$http', ($scope, $routeParams, $http) => {
     $scope.instrumentType = $routeParams.type;
     console.log($scope.instrumentType);
     $scope.instruments = [];
-    $http.get('https://rocky-citadel-32862.herokuapp.com/MusicalInstruments/instruments').then((data)=>{
-        $scope.instruments=data;
+    $http.get('https://rocky-citadel-32862.herokuapp.com/MusicalInstruments/instruments').then((data) => {
+        $scope.instruments = data;
         console.log($scope.instruments.data);
+    })
+}])
+
+app.controller('instrument-controller', ['$scope', '$routeParams', '$http', ($scope, $routeParams, $http) => {
+    $scope.instrumentType = $routeParams.type;
+    console.log($scope.instrumentType);
+    $scope.instrument = null;
+    $http.get('https://rocky-citadel-32862.herokuapp.com/MusicalInstruments/instruments').then((data) => {
+        let tmp = data.data;
+        console.log(tmp);
+        for(let item of tmp){
+            if(item.id===parseInt($routeParams.id)){
+                $scope.instrument=item;
+            }
+        }
+        console.log($scope.instrument);
     })
 }])
